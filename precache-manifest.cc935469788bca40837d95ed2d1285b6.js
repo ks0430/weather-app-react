f44fae007230d6e10011552815a6e720047b9b07
Evelyn <ks0430@163.com>
2019-02-24 14:47:12 +1000

    Updates

---
+++ precache-manifest.cc935469788bca40837d95ed2d1285b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a69d6614ab32b008890e",
+    "url": "/weather-app-react/static/js/runtime~main.a69d6614.js"
+  },
+  {
+    "revision": "c854ddb18c807d669ec2",
+    "url": "/weather-app-react/static/js/main.c854ddb1.chunk.js"
+  },
+  {
+    "revision": "90e77fc2ac12863db8e6",
+    "url": "/weather-app-react/static/js/1.90e77fc2.chunk.js"
+  },
+  {
+    "revision": "c854ddb18c807d669ec2",
+    "url": "/weather-app-react/static/css/main.bbc1b897.chunk.css"
+  },
+  {
+    "revision": "f5839b911de6fce6133c37bf6ffb0db0",
+    "url": "/weather-app-react/index.html"
+  }
+];
